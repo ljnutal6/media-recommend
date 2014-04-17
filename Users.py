60255d2d8c90fc9d2d22c633a1177333d0ff44d8
ljnutal6 <odolwa@q.com>
2014-04-17 17:17:58 -0600

    Made methods static

--- Users.py
+++ Users.py
@@ -1,34 +1,32 @@
 import pymongo
 from pymongo import MongoClient
 
-class Users:
-    def __init__(self):
-        self.client = MongoClient()
-        self.db = self.client.database
-        self.collection = self.db.userCollection
+client = MongoClient()
+db = client.database
+collection = db.userCollection
 
-    def add_user(self, username, password, email):
-        if email is None:
-            email = ""
+def add_user(username, password, email):
+	if email is None:
+		email = ""
         
-        user = {"username": username,
-                "password": password,
-                "email": email,
-                "favorites": []}
-        user_id = self.collection.insert(user)
-        return user_id
+	user = {"username": username,
+		"password": password,
+		"email": email,
+		"favorites": []}
+	user_id = collection.insert(user)
+	return user_id
     
-    def find(self, user_id):
-        return self.collection.find_one({"_id": user_id})
+def find(user_id):
+	return collection.find_one({"_id": user_id})
     
-    def add_favorite(self, user_id, favorite_id):
-        user = self.find(user_id)
-        user["favorites"].append(favorite_id)
-        self.collection.save(user)
+def add_favorite(user_id, favorite_id):
+	user = find(user_id)
+	user["favorites"].append(favorite_id)
+	collection.save(user)
     
-    def remove_favorite(self, user_id, favorite_id):
-        user = self.find(user_id)
-        favorites = user["favorites"]
-        if favorite_id in favorites: 
-            favorites.remove(favorite_id)
-        self.collection.save(user)
+def remove_favorite(user_id, favorite_id):
+	user = find(user_id)
+	favorites = user["favorites"]
+	if favorite_id in favorites: 
+		favorites.remove(favorite_id)
+	collection.save(user)
