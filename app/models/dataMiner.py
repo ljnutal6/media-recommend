9937d9a85ac9583ee4f58e850a2f38f779fc41a4
ljnutal6 <odolwa@q.com>
2014-04-19 13:34:30 -0600

    Create dataMiner.py
    New version of dataMiner. Now works with the database rather than the stubs class

---
+++ app/models/dataMiner.py
@@ -0,0 +1,112 @@
+from __future__ import division
+import Users
+import Media
+import Rule
+
+#Arbitrary parameters. Can be adjusted up or down to more or fewer rules that are less impressive.
+# Value of 1 is so picky that it rejects every rule that isn't supported by every dataset
+# Value of 0 is so permissive that it finds every association that occurs even once.
+minSupport = 0.26
+minConf = 0.3
+
+def frequentItemSets():
+	itemSets = []
+	firstRound = []
+	mostRecentRound = []
+	items = Media.getMediaIDs()
+	candidates = []
+	for item in items:
+		candidates.append([item])
+	round = 0
+	while candidates:
+		mostRecentRound = []
+		for candidate in candidates:
+			if support(candidate) > minSupport:
+				itemSets.append(candidate)
+				mostRecentRound.append(candidate)
+				if round == 0:
+					firstRound.append(candidate)
+		candidates = genNewCandidates(firstRound, mostRecentRound, round)
+		round = round + 1
+	return itemSets
+
+def support(itemset):
+	count = 0
+	users = Users.getUserIDs()
+	userCount = len(users)
+	for user in users:
+		missing = False
+		for item in itemset:
+			if Users.userLikes(user, item):
+				missing = missing
+			else:
+				missing = True
+		if not missing:
+			count = count + 1
+	#print str(count/userCount)
+	#print str(count)	
+	return count / userCount
+
+def confidence(lhs, rhs):
+	supp = support(lhs)
+	lhs.append(rhs)
+	supportUnion = support(lhs)
+	return supportUnion/supp
+
+def genRulesets(itemset):
+	ruleset = []
+	if len(itemset) < 2:
+		return ruleset
+	for item in itemset:
+		tempset = list(itemset)
+		tempset.remove(item)
+		conf = confidence(tempset, item)
+		tempset.remove(item)
+		if conf > minConf:
+			ruleset.append((tempset, item, conf))
+	return ruleset
+
+def Rulesets(itemset):
+	rules = []
+	for set in itemset:
+		r = genRulesets(set)
+		rules.extend(r)
+	return rules
+
+def genNewCandidates(firstRound, mostRecentRound, round):
+	#print "firstround:"
+	#for item in firstRound:
+		#print str(item)
+	#print "recentround:"
+	#for item in mostRecentRound:
+		#print str(item)
+	dround = round + 1
+	candidates = []
+	for solo in firstRound:
+		for set in mostRecentRound:
+			if isNotPartOf(solo[0], set):
+				z = list(set)
+				z.append(solo[0])
+				if len(z) > dround and isSorted(z):
+					candidates.append(z)
+	return candidates
+
+def isNotPartOf(newMember, oldList):
+	for num in oldList:
+		if newMember == num:
+			return False
+	return True
+
+
+
+def isSorted(list):
+	for i in range(0,len(list)-1):
+		if list[i] > list[i+1]:
+			return False
+	return True
+
+def dataMine():
+	Rule.remove_all()
+	rules = Rulesets(frequentItemSets())
+	for rule in rules:
+		Rule.add_rule(rule[0],rule[1],rule[2])
