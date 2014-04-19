07f4e69df146c34e681b2022afe9841fecf7cc7c
ljnutal6 <odolwa@q.com>
2014-04-19 12:17:50 -0600

    Create Users.py
    Couldn't get the static methods to work in the user class

---
+++ app/models/Users.py
@@ -0,0 +1,49 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.database
+collection = db.userCollection
+
+def add_user(username, password, email):
+	if email is None:
+		email = ""
+        
+	user = {"username": username,
+		"password": password,
+		"email": email,
+		"favorites": []}
+	user_id = collection.insert(user)
+	return user_id
+    
+def get(user_id):
+	return collection.find_one({"_id": user_id})
+	
+def validate(username, password):
+   	users = collection.find({"username": username, "password": password}).count()
+	if users == 1:
+		return True
+	else:
+		return False
+		
+def get_userID(username, password):
+	users = collection.find_one({"username": username, "password": password})
+	return users["_id"]		
+    
+def add_favorite(user_id, favorite_id):
+	user = find(user_id)
+	user["favorites"].append(favorite_id)
+	collection.save(user)
+    
+def remove_favorite(user_id, favorite_id):
+	user = find(user_id)
+	favorites = user["favorites"]
+	if favorite_id in favorites: 
+		favorites.remove(favorite_id)
+	collection.save(user)
+
+def getUserIDs():
+	ids = []
+	for item in collection.find():
+		ids.append(item["_id"])
+	return ids
