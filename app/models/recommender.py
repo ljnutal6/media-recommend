74b5e8d4fa404154f9cfa0c23065cf0b9ea75d9f
ljnutal6 <odolwa@q.com>
2014-04-19 14:54:54 -0600

    Added recomendations by type

--- app/models/recommender.py
+++ app/models/recommender.py
@@ -32,5 +32,37 @@ def recommend_anon(mediaIDs):
 		if applicable:
 			recommendations.append(rule[1])
 	return recommendations
+	
+def recommend_by_type(user, mediaType):
+	rules = Rule.getRules()
+	rules = sorted(rules, key=lambda x: x[2], reverse=True)
+	recommendations = []
+	for rule in rules:
+		preconditions = rule[0]
+		applicable = True
+		for cond in preconditions:
+			if not Users.userLikes(user, cond):
+				applicable = False
+		if Users.userLikes(user, rule[1]):
+			applicable = False
+		if applicable and Media.isType(rule[1], mediaType):
+			recommendations.append(rule[1])
+	return recommendations
+
+def recommend_anon_by_type(mediaIDs, mediaType):
+	rules = Rule.getRules()
+	rules = sorted(rules, key=lambda x: x[2], reverse=True)
+	recommendations = []
+	for rule in rules:
+		preconditions = rule[0]
+		applicable = True
+		for cond in preconditions:
+			if not cond in mediaIDs:
+				applicable = False
+		if rule[1] in mediaIDs:
+			applicable = False
+		if applicable and Media.isType(rule[1], mediaType):
+			recommendations.append(rule[1])
+	return recommendations
 
 #recommend(4)
