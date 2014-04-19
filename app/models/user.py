9c900c1ae54364cdd00e08367922d255f2fd9962
joshbcondie <condiej@fishing.cs.byu.edu>
2014-04-19 12:16:28 -0600

    Added testing code without database

--- app/models/user.py
+++ app/models/user.py
@@ -2,14 +2,14 @@ import pymongo
 from pymongo import MongoClient
 
 class User():
-    client = MongoClient()
-    db = client.database
-    collection = db.userCollection
+    #client = MongoClient()
+    #db = client.database
+    #collection = db.userCollection
     
     def __init__(self, record):
-        self.username = record.username
-        self.password = record.password
-        self.favorites = record.favorites
+        self.username = record["username"]
+        self.password = record["password"]
+        self.favorites = record["favorites"]
     
     def get_id(self):
         return self.username
@@ -27,32 +27,35 @@ class User():
         collection.update({"username":self.username},{"$set":{"favorites",self.favorites}})
     
     #Static methods
+    @staticmethod
     def add_user(username, password):
         user = {"username": username,
           "password": password,
           "favorites": []}
         user_id = collection.insert(user)
         return user_id
-        
+    @staticmethod        
     def get(user_id):
-        return User(collection.find_one({"_id": user_id}))
-      
+        return User({"username":user_id, "password":"abc", "favorites":[]})
+        #return User(collection.find_one({"_id": user_id}))
+    @staticmethod      
     def validate(username, password):
-        users = collection.find({"username": username, "password": password}).count()
-        if users == 1:
-		        return True
-        else:
-    	  	  return False
-        
+        return True        
+        #users = collection.find({"username": username, "password": password}).count()
+        #if users == 1:
+		    #    return True
+        #else:
+    	  #	  return False
+    @staticmethod        
     def get_userID(username, password):
         users = collection.find_one({"username": username, "password": password})
         return users["_id"]
-
+    @staticmethod
     def add_favorite(user_id, favorite_id):
         user = find(user_id)
         user["favorites"].append(favorite_id)
         collection.save(user)
-
+    @staticmethod
     def remove_favorite(user_id, favorite_id):
         user = find(user_id)
         favorites = user["favorites"]
