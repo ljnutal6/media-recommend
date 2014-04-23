b11151ee6776b595a7b1ed9b0f3dda98585108c0
Josh <joshbcondie@gmail.com>
2014-04-23 17:28:39 -0600

    Anonymous users may view items but not edit them

--- app/views.py
+++ app/views.py
@@ -52,21 +52,25 @@ def mine_if_necessary():
     return redirect(url_for("index"))	
 
 @app.route("/Media.html", methods=["GET","POST"])
-@login_required
 def media():
     try:
+        if current_user.get_id():
+            name = current_user.username
+        else:
+            name = None
+        
         if request.method == "POST":
             print "POST"
             for input in request.form:
                 if input != "name" and input != "type" and input != "last" and request.form[input] != "":
                     update(getID(request.form["name"], request.form["type"]), input.replace("_", " "), request.form[input])
-            return render_template("Media.html", media=find(getID(request.form["name"],request.form["type"])), last=request.form["last"])
+            if getID(request.form["name"],request.form["type"], False) == None:
+                raise
+            return render_template("Media.html", name=name, media=find(getID(request.form["name"],request.form["type"], False)), last=request.form["last"])
         else:
-            print "GET"
-            print request.args["name"]
-            print request.args["type"]
-            print request.args["last"]
-            return render_template("Media.html", media=find(getID(request.args["name"],request.args["type"])), last=request.args["last"])
+            if getID(request.args["name"],request.args["type"], False) == None:
+                raise
+            return render_template("Media.html", name=name, media=find(getID(request.args["name"],request.args["type"], False)), last=request.args["last"])
     except:
         if "last" in request.args:
             if request.args["last"] == "Suggestions.html":
